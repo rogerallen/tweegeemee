8f5b7fbf4342ed3b7cf3a0e8258cb270fa044a85
Roger Allen <rallen@gmail.com>
2015-05-31 10:09:43 -0700

    Twitter hooked up.

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [twitter-api "0.7.8"]
+                 [environ "1.0.0"]]
+  :plugins [[lein-environ "1.0.0"]])
--- src/tweegeemee/core.clj
+++ src/tweegeemee/core.clj
@@ -1,6 +1,46 @@
-(ns tweegeemee.core)
+(ns tweegeemee.core
+  (:require [twitter.api.restful :as twitter]
+            [twitter.oauth :as twitter-oauth]
+            [twitter.request :as twitter-request]
+            [environ.core :refer [env]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def my-creds (twitter-oauth/make-oauth-creds
+               (env :app-consumer-key)
+               (env :app-consumer-secret)
+               (env :user-access-token)
+               (env :user-access-secret)))
+
+(comment
+
+  ;; got some info about me
+  (twitter/users-show :oauth-creds my-creds
+                      :params {:screen-name "tweegeemee"})
+
+  (def last-status-id (-> (twitter/users-show :oauth-creds my-creds
+                                              :params {:screen-name "tweegeemee"})
+                          :body
+                          :status
+                          :id))
+  last-status-id
+
+  ;; get statuses & favorites
+  (defn print-last-n-statuses [N]
+    (doall (doseq [s (:body (twitter/statuses-user-timeline
+                             :oauth-creds my-creds
+                             :params {:count N
+                                      :screen-name "tweegeemee"}))]
+             (println (:retweet_count s) (:favorite_count s) (:text s)))))
+  (print-last-n-statuses 5)
+
+  ;; coolio
+  (twitter/statuses-update
+   :oauth-creds my-creds
+   :params {:status "Testing a post from the REPL."})
+
+  ;; double-coolio
+  (twitter/statuses-update-with-media
+   :oauth-creds my-creds
+   :body [(twitter-request/file-body-part "test0004.png")
+          (twitter-request/status-body-part "An image to start with")])
+
+  )
