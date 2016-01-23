fb6751c9928100099df6e721ad81b9cf0d29f0cb
Roger Allen <rallen@gmail.com>
2016-01-23 13:01:33 -0800

    bump versions.  v1.3.4
    update clisk & clojure versions.  1.8.0 clojure doesn't work for some reason.
    also allow repl to run with fewer constraints on memory.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject tweegeemee "1.3.3"
+(defproject tweegeemee "1.3.4"
   :description  "tweegeemee is an experiment in image creation and breeding via twitter."
   :url          "http://github.com/rogerallen/tweegeemee"
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [twitter-api         "0.7.8"]
                  [environ             "1.0.0"]
-                 [net.mikera/clisk    "0.10.0"]
+                 [net.mikera/clisk    "0.11.0"]
                  [tentacles           "0.3.0"]
                  [clj-time            "0.11.0"]]
   :main         ^:skip-aot tweegeemee.core
@@ -15,11 +15,12 @@
   ;; Enable full optimizer, don't let heap or metaspace get too big
   ;; 260+140=400 allowing 112mb for the process
   ;; finally adding the max direct mem size.  see what that does
-  ;; FIXME -- control this via a different profile
   :jvm-opts     ^:replace ["-Xss512k" "-Xms128m" "-Xmx260m"
                            "-XX:MaxMetaspaceSize=140m"
                            "-XX:MaxDirectMemorySize=50m"]
-
   :target-path  "target/%s"
-  :profiles     {:uberjar {:aot :all}}
+  :profiles     {:uberjar {:aot :all}
+                 ;; lein repl enables full optimizer, no limits
+                 :repl    {:jvm-opts ^:replace []}
+                 }
   )
